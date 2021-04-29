1281246003466484d78ae3544e14bee61904c269
viviane <v.buschwallace@yahoo.com>
2021-04-29 15:46:27 +0200

    added variables and alert message

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,12 @@
+// Notice to user about how to navigate
+alert("To navigate: w = up, s = down, d = right, a = left");
+
 // What is Q key code? so here all the keys code you need to know
 // https://keycode.info/
 let hStandPoint = 0;
 let vStandPoint = 0;
+const goalheight = 50;
+const goalwidth = 50;
 function goUp() {
   vStandPoint -= 10;
   mini.style.top = vStandPoint + "px";
@@ -27,6 +32,12 @@ function move(e) {
   if (e.code == "KeyS") goDown();
   console.log(hStandPoint);
   console.log(vStandPoint);
+  //   if (
+  //     window.innerHeight / 2 <= vStandPoint &&
+  //     window.innerWidth - 100 <= hStandPoint
+  //   )
+  //     alert("cool you are winner");
+  // }
   if (
     window.innerHeight / 2 <= vStandPoint &&
     window.innerWidth - 100 <= hStandPoint
